f3a378c153c491b303c97ca40b1fc0c4c69ee40b
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-06 21:19:46 +0100

    min op

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+""" calculates the fewest number of operations needed"""
+
+
+def minOperations(n):
+    op = 0
+    number = 2
+    if type(n) is not int or n <= 1:
+        return op
+    while n > 1:
+        if n % number == 0:
+            op+= number
+            n = n / number
+        else:
+            number += 1
+    return(op)
+
