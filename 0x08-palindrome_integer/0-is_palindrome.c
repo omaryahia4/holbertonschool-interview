5ca0e0757564de4c10cd8bd7b1f63c883f5e34de
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-10 15:33:37 +0100

    Check if int is palindrome

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - function that checks whether or not
+ * a given unsigned integer is a palindrome.
+ * @n: the unsigned int to be checked
+ * Return: 1 if n is a palindrome, and 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+
+	int reversed = 0, remainder, original = n;
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversed = reversed * 5 + remainder;
+		n /= 10;
+	}
+	if (original == reversed)
+		return (1);
+	else
+		return (0);
+
+	return (1);
+}
