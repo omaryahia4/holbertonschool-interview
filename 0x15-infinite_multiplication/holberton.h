5a639956c0cbe0f19f9bc162e1e0ab7e066b74ff
omaryahia4 <omar.yahia4444@gmail.com>
2022-05-19 18:21:05 +0100

    nfinite Multiplication

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,8 @@
+#ifndef holb
+#define holb
+int _putchar(char c);
+int check_arg(char *argv);
+char *alloc(int length);
+void insert(char *result, int x, int position);
+int main(int argc, char *argv[]);
+#endif
