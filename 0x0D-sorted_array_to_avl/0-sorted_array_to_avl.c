86e58f52ec641dca1c1044c70d4efb397be3aa3a
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-06 16:38:33 +0100

    AVL tree from an array

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -2,71 +2,71 @@
 
 /**
 * sorted_array_to_avl -function that builds an AVL tree from an array
-* @array: int
-* @size: int
+* @array: array
+* @size: size of array
 * Return: pointer to the root node of the created AVL tree, or NULL
 */
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *tree;
+	avl_t *tree;
 
-    if (!array || size <= 0)
-        return (NULL);
+	if (!array || size <= 0)
+		return (NULL);
 
-    tree = add_noeud(0, NULL, size - 1, array);
-        return (tree);
+	tree = add_node(0, NULL, size - 1, array);
+		return (tree);
 }
 
 
 /**
-* add_noeud - create tree
-* @array: int
-* @debut: int
-* @fin: int
-* @parent: int
+* add_node - create tree
+* @array: array
+* @s: start node
+* @f: final node
+* @parent: parent node
 * Return: pointeur(mean true)
 */
-avl_t *add_noeud(size_t d, avl_t *parent, size_t f, int *array)
+avl_t *add_node(size_t s, avl_t *parent, size_t f, int *array)
 {
-    size_t m;
-    avl_t *tree;
+	size_t m;
+	avl_t *tree;
 
-    if (d > f)
-        return (NULL);
+	if (s > f)
+		return (NULL);
 
-    m = (d + f) / 2;
-    tree = new_noeud(parent, array[m]);
-    if (!tree)
-        return (NULL);
+	m = (s + f) / 2;
+	tree = new_node(parent, array[m]);
+	if (!tree)
+		return (NULL);
 
-    if (m != f)
-        tree->right = add_noeud(m + 1, tree, f, array);
+	if (m != f)
+		tree->right = add_node(m + 1, tree, f, array);
 
-    if (m != d)
-        tree->left = add_noeud(d, tree, m - 1, array);
+	if (m != s)
+		tree->left = add_node(s, tree, m - 1, array);
 
-    return (tree);
+	return (tree);
 }
 
 /**
-* new_noeud - creation de nouveau noeud
-* @parent: pointeur
-* @n: int
-* Return: noeud(mean true)
+* new_node - create new node
+* @p: parent node
+* @n: data
+* Return: node
 */
-avl_t *new_noeud(avl_t *p, size_t n)
+avl_t *new_node(avl_t *p, size_t n)
 {
-    avl_t *new;
+	avl_t *new;
 
-    new = malloc(sizeof(avl_t));
+	new = malloc(sizeof(avl_t));
 
-    if (!new)
-        return (NULL);
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    new->right = NULL;
-    new->parent = p;
-    new->left = NULL;
-    return (new);
+	new->n = n;
+	new->right = NULL;
+	new->parent = p;
+	new->left = NULL;
+	return (new);
 }
