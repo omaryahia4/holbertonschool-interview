25778fb12180c249304debdbf825aa503cddf0e9
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-09 16:34:04 +0100

    Merge sort

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+void display(int *array, int *right, int li, int *left, int ri, size_t size);
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 
