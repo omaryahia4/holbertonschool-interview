c19a4fa8177d463c5af1587896e12f373774f99b
omaryahia4 <omar.yahia4444@gmail.com>
2022-06-23 21:16:05 +0100

    making change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Change comes from within
-"""
+"""Coins"""
 
 
 def makeChange(coins, total):
-    """
-Given a pile of coins of different values,
-determine the fewest number of coins needed to meet a given amount total.
+    """Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total.
     """
     if total <= 0:
         return 0
